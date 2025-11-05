e4a1e130929ce0c687ec688a9fecf94e64e26926
ShubhamDeshmuk-h <shubhamdeshmukh3251@gmail.com>
2025-11-06 02:06:52 +0530

    stable version 1.0

--- app.config.js
+++ app.config.js
@@ -11,8 +11,12 @@ module.exports = ({ config }) => {
   return {
     ...config,
     extra: {
-      EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL,
-      EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-    },
+      ...config.extra,
+      EXPO_PUBLIC_SUPABASE_URL: process.env.EXPO_PUBLIC_SUPABASE_URL || 'https://kihgmhodhbwaocugeygz.supabase.co',
+      EXPO_PUBLIC_SUPABASE_ANON_KEY: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtpaGdtaG9kaGJ3YW9jdWdleWd6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjIzNjcwNDMsImV4cCI6MjA3Nzk0MzA0M30.kkb2DR1KfLtIrnNdgxyDMN9xJBnOdSJqENxPZsazB2w',
+      eas: {
+        projectId: 'e75a47f0-98e0-4a46-becc-0caf782343dc'
+      }
+    }
   };
 };
